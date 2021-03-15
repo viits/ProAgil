af0275f8119dc14adbf5cf283da086f3be921825
Esdras Victor <toyobr@hotmail.com>
2021-03-15 13:38:22 -0300

    Validation using Annotation

--- ProAgil.API/Dtos/EventoDto.cs
+++ ProAgil.API/Dtos/EventoDto.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProAgil.API.Dtos
 {
     public class EventoDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="Campo obrigatório")]
+        [StringLength(100,MinimumLength= 3, ErrorMessage="Local é entre 3 a 100 caracteres")]
         public string Local { get; set; }
         public string DataEvento { get; set; }
+        [Required(ErrorMessage="O tema deve ser preenchido")]
         public string Tema { get; set; }
+        [Range(1,120000,ErrorMessage="Quantidade de pessoas é entre 1 e 120000")]
         public int QtPessoas { get; set; }
         public string ImagemUrl { get; set;}
+        [Phone]
         public string Telefone{get;set;}
+        [EmailAddress(ErrorMessage="Email Invalido")]
         public string Email { get; set; }
         public string Lote { get; set; }
         public List<LoteDto> Lotes { get; set; }
--- ProAgil.API/Dtos/RedeSocialDto.cs
+++ ProAgil.API/Dtos/RedeSocialDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProAgil.API.Dtos
 {
     public class RedeSocialDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Url { get; set; }
 
     }
